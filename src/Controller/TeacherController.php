5e55ac9a8fa7975f68fed26cac97f94ac1f9d856
Ibtissem Lengliz <ibtilengliz@gmail.com>
2021-03-12 22:36:31 +0100

    Initial commit

---
+++ src/Controller/TeacherController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use phpDocumentor\Reflection\Type;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TeacherController extends AbstractController
+{
+    /**
+     * @Route("/teacher", name="teacher")
+     */
+    public function index(): Response
+    {
+        return $this->render('teacher/index.html.twig', [
+            'controller_name' => 'TeacherController',
+        ]);
+    }
+
+
+}
